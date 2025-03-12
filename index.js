6dff48ad8b05cd85ddc40f83bea8a4ca8a953c03
Marycharity <marycharity5@gmail.com>
2025-03-12 11:57:10 +0300

    Complete test

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(function (tutorial) {
+    return tutorial
+      .split(" ") // Split the sentence into words
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize first letter
+      .join(" "); // Join words back into a sentence
+  });
+};
+
